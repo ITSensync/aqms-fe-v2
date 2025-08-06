d22b070790933b7f435002885f7ddeb47659e4da
alief abdillah <aliefmabdillah09@gmail.com>
2025-08-06 16:49:34 +0700

    feat: slicing center gauge diagram

--- src/app/components/CenterParameter.tsx
+++ src/app/components/CenterParameter.tsx
@@ -1,44 +1,42 @@
 "use client";
 import React from "react";
-import dynamic from "next/dynamic";
-const GaugeComponent = dynamic(() => import("react-gauge-component"), {
-  ssr: false,
-});
+import GaugeDiagram from "./GaugeDiagram";
 
 export default function CenterParameter() {
   return (
-    <div className="w-1/2">
-      <GaugeComponent
-        className="w-[16vw] -mt-10"
-        // type="radial"
-        arc={{ width: 0.2 }}
-        labels={{
-          valueLabel: {
-            formatTextValue: (value) => value + " W/m3",
-            style: {
-              fontSize: 24,
-              fill: "#0F67B1",
-              fontWeight: "bold",
-              textShadow: "",
-            },
-          },
-          tickLabels: {
-            type: 'outer',
-            defaultTickValueConfig: {
-              formatTextValue: (value) => value,
-              style: {
-                fill: "#0F67B1",
-                fontSize: 8,
-              }
-            },
-            defaultTickLineConfig: {
-              length: 5,
-              color: "#0567B1"
-            }
-          }
-        }}
-      />
-      <p className="font-sf-pro text-blue-darkest font-bold text-center ml-10 -mt-2">Solar Radiasi</p>
+    <div className="flex flex-col gap-14">
+      <div className="flex flex-row mt-1">
+        <div className="w-1/2 -ml-1">
+          <GaugeDiagram />
+        </div>
+        <div className="w-1/2 -ml-6">
+          <GaugeDiagram />
+        </div>
+      </div>
+      <div className="flex flex-row">
+        <div className="w-1/2 -ml-1">
+          <GaugeDiagram />
+        </div>
+        <div className="w-1/2 -ml-6">
+          <GaugeDiagram />
+        </div>
+      </div>
+      <div className="flex flex-row">
+        <div className="w-1/2 -ml-1">
+          <GaugeDiagram />
+        </div>
+        <div className="w-1/2 -ml-6">
+          <GaugeDiagram />
+        </div>
+      </div>
+      <div className="flex flex-row">
+        <div className="w-1/2 -ml-1">
+          <GaugeDiagram />
+        </div>
+        <div className="w-1/2 -ml-6">
+          <GaugeDiagram />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/app/components/GaugeDiagram.tsx
@@ -0,0 +1,94 @@
+"use client";
+import React from "react";
+import dynamic from "next/dynamic";
+const GaugeComponent = dynamic(() => import("react-gauge-component"), {
+  ssr: false,
+});
+
+export default function GaugeDiagram({
+  value = "30",
+  unit = "W/m3",
+  name = "Lorem",
+}: {
+  value?: string;
+  unit?: string;
+  name?: string;
+}) {
+  return (
+    <>
+      <GaugeComponent
+        className="w-[10vw] ml-10 -mt-9"
+        type="radial"
+        arc={{
+          gradient: true,
+          width: 0.25,
+          colorArray: ["#5BE12C", "#EA4228"],
+          subArcs: [
+            {
+              color: "#EA4228",
+              // showTick: true,
+            },
+            {},
+            {},
+            // {
+            //   // limit: 37,
+            //   // color: "#F5CD19",
+            //   // showTick: true,
+            // },
+            // {
+            //   // limit: 58,
+            //   // color: "#5BE12C",
+            //   // showTick: true,
+            // },
+            // {
+            //   // limit: 75,
+            //   // color: "#F5CD19",
+            //   // showTick: true,
+            // },
+            { color: "#EA4228" },
+          ],
+        }}
+        pointer={{
+          length: 0.65,
+          color: "#001e2d",
+          elastic: true,
+          animationDelay: 0,
+          width: 15,
+        }}
+        labels={{
+          valueLabel: {
+            formatTextValue: () => "",
+            style: {
+              fontSize: 24,
+              fill: "#0F67B1",
+              fontWeight: "bold",
+              textShadow: "",
+            },
+          },
+          tickLabels: {
+            hideMinMax: true,
+            type: "outer",
+            defaultTickValueConfig: {
+              formatTextValue: (value) => value,
+              style: {
+                fill: "#0F67B1",
+                fontSize: 8,
+              },
+            },
+            defaultTickLineConfig: {
+              length: 5,
+              color: "#0567B1",
+            },
+          },
+        }}
+      />
+      <div className="font-sf-pro-rounded text-blue-darkest flex flex-col justify-center items-center ml-6 -mt-6">
+        <div className="flex flex-row items-end gap-0.5">
+          <p className="font-black text-md">{value}</p>
+          <p className="text-xs">{unit}</p>
+        </div>
+        <p className="text-md font-semibold text-center">{name}</p>
+      </div>
+    </>
+  );
+}
