60c5b127f41bc03a463085aab8e0871bcf7e55b5
alief abdillah <aliefmabdillah09@gmail.com>
2025-08-06 17:24:02 +0700

    feat: new env variable

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    AQMS_TYPE: process.env.AQMS_TYPE,
+  }
 };
 
 export default nextConfig;
