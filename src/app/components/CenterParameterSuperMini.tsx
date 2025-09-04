4f76d1d3d67ac13f53bf7d365b5f2772879cbbe4
alief abdillah <aliefmabdillah09@gmail.com>
2025-09-04 22:03:56 +0700

    feat: new bg

--- src/app/components/CenterParameterSuperMini.tsx
+++ src/app/components/CenterParameterSuperMini.tsx
@@ -10,7 +10,7 @@ export default function CenterParameterSuperMini({
   return (
     <div className="flex flex-col mt-42">
       <div className="flex flex-col gap-16">
-        <GaugeDiagram name="PM10" unit="μg/m3" value={sensorData.pm10}/>
+        <GaugeDiagram name="PM25" unit="μg/m3" value={sensorData.pm25}/>
       </div>
     </div>
   )
